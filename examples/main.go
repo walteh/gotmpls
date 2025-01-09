5f4ecfa6893be4faa6ffd9ededdb5f80685d328f
Walter Scott <w@lter.ca>
2025-01-09 06:25:02 -0500

    diagnostics working, gtp

--- examples/main.go
+++ examples/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"context"
-	"os"
-	"runtime"
-
-	"github.com/rs/zerolog"
-)
-
 func main() {
-	logger := zerolog.New(os.Stderr).With().
-		Timestamp().
-		Str("app", "go-tmpl-typer").
-		Str("os", runtime.GOOS).
-		Logger()
 
-	ctx := logger.WithContext(context.Background())
-	zerolog.Ctx(ctx).Info().Str("status", "starting").Msg("application initialized")
 }
