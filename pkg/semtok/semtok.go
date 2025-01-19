b2924b7d6d5580e9d78c6fb38f286ed177abb21d
Walter Scott <w@lter.ca>
2025-01-19 07:17:16 -0500

    add semtok package, atp

---
+++ pkg/semtok/semtok.go
@@ -0,0 +1,15 @@
+package semtok
+
+import (
+	"context"
+
+	"github.com/walteh/gotmpls/pkg/position"
+)
+
+func GetTokensForFile(ctx context.Context, content []byte) ([]uint32, error) {
+	return nil, nil
+}
+
+func GetTokensForRange(ctx context.Context, content []byte, ranged *position.RawPosition) ([]uint32, error) {
+	return nil, nil
+}
