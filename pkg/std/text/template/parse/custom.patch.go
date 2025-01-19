bec50c8e779d3cbede967a33212a2b8f39acf39d
Walter Scott <w@lter.ca>
2025-01-19 14:13:54 -0500

    remove unused sd lib stuff

--- pkg/std/text/template/parse/custom.patch.go
+++ pkg/std/text/template/parse/custom.patch.go
@@ -1,56 +1,5 @@
 package parse
 
-type DelimitedNode interface {
-	Delims() (item, item)
-}
-
-// pure go
-// var _ DelimitedNode = (*ActionNode)(nil)
-// var _ DelimitedNode = (*CommentNode)(nil)
-var _ DelimitedNode = (*BranchNode)(nil)
-
-// less pure
-
-var _ DelimitedNode = (*BreakNode)(nil)
-var _ DelimitedNode = (*ContinueNode)(nil)
-var _ DelimitedNode = (*elseNode)(nil)
-var _ DelimitedNode = (*EndNode)(nil)
-var _ DelimitedNode = (*NilNode)(nil)
-var _ DelimitedNode = (*TemplateNode)(nil)
-var _ DelimitedNode = (*BranchNode)(nil)
-var _ DelimitedNode = (*WithNode)(nil)
-var _ DelimitedNode = (*RangeNode)(nil)
-var _ DelimitedNode = (*IfNode)(nil)
-
-// func (me *ActionNode) Delims() (item, item)  { return me.keyword, me.keyword }
-// func (me *CommentNode) Delims() (item, item) { return me.keyword, me.keyword }
-func (me *BranchNode) Delims() (item, item) { return me.keyword, me.keyword }
-
-func (me *BreakNode) Delims() (item, item)    { return me.keyword, me.keyword }
-func (me *ContinueNode) Delims() (item, item) { return me.keyword, me.keyword }
-func (me *elseNode) Delims() (item, item)     { return me.keyword, me.keyword }
-func (me *EndNode) Delims() (item, item)      { return me.keyword, me.keyword }
-func (me *NilNode) Delims() (item, item)      { return me.keyword, me.keyword }
-func (me *TemplateNode) Delims() (item, item) { return me.keyword, me.keyword }
-
-func (t *Tree) lastItemReadOnly() item {
-	t.lex.backup()
-	item := t.lex.item
-	t.lex.next()
-	return item
-}
-
-func (t *Tree) nextItemReadOnly() item {
-	t.lex.next()
-	item := t.lex.item
-	t.lex.backup()
-	return item
-}
-
-func (t *Tree) thisItemReadOnly() item {
-	return t.lex.item
-}
-
 type KeywordNode interface {
 	Keyword() item
 }
